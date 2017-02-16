eb0400ff6e24d35ebf9ef95226241da1405992be
andrekas <alaurimaa@gmail.com>
2017-02-16 15:16:21 +0200

    VisualStudio failid

---
+++ XPProject.Tests/WebpageAddressTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Security.Policy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace XPProject.Tests
+{
+    [TestClass]
+    public class WebpageAddressTests
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            Assert.IsNotNull(new WebpageAddress());
+        }
+
+        [TestMethod]
+        public void UrlTest() {
+            var w = new WebpageAddress();
+            var u = new Url("www.abc.ee");
+            
+            Assert.AreNotEqual(String.Empty, u);
+            
+
+
+
+        }
+        
+    }
+}
---
+++ XPProject/Archetype.cs
@@ -0,0 +1,30 @@
+﻿namespace XPProject
+{
+    public class Archetype
+    {
+        public class WebpageAddress
+        {
+            public string Url { get; set; }
+        }
+
+        public class EmailAddress
+        {
+            public string Email { get; internal set; }
+        }
+
+        public class TelekomAddress
+        {
+            public string Telekom { get; set; }
+        }
+
+        public class GeographicalAddress
+        {
+            
+        }
+
+        public class RelationshipType
+        {
+            
+        }
+    }
+}
