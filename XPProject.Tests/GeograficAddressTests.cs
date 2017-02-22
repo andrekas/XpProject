3dfc7523d180e1cb1df9c575720fc6f2fed0bfd4
Angelina <angelina.polschikova@gmail.com>
2017-02-23 00:45:10 +0200

    Added new folders, classes and some tests

---
+++ XPProject.Tests/GeograficAddressTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace XPProject.Tests
+{
+    [TestClass]
+    public class GeograficAddressTests
+    {
+        private GeograficAddress g;
+        [TestInitialize]
+        public void Init() { g = new GeograficAddress(); }
+        [TestMethod]
+        public void ConstructorTest() { Assert.IsNotNull(g); }
+
+        private string addressLines;
+        
+        [TestMethod]
+        public void AddressLinesTest()
+        {
+           
+        }
+    }
+}
---
+++ XPProject/Classes/WebpageAddress.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace XPProject
+{
+    public class WebpageAddress
+    {
+        public string Url { get; set; }
+    }
+}
