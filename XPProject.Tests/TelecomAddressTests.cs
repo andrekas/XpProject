433e49a36283897e36442c63cfca1ac497457943
Kert Kuusalu <kert.kuusalu@hotmail.com>
2017-02-23 09:59:59 +0200

    a

--- XPProject.Tests/TelecomAddressTests.cs
+++ XPProject.Tests/TelecomAddressTests.cs
@@ -11,59 +11,18 @@ namespace XPProject.Tests
     [TestClass]
     public class TelecomAddressTests
     {
-        public TelecomAddressTests()
+        private TelecomAddress g;
+        [TestInitialize]
+        public void Init() { g = new TelecomAddress(); }
+        [TestCleanup]
+        public void Cleanup()
         {
-            //
-            // TODO: Add constructor logic here
-            //
+            g = null;
         }
-
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
 
         [TestMethod]
-        public void TestMethod1()
-        {
-            //
-            // TODO: Add test logic here
-            //
-        }
+        public void ConstructorTest() { Assert.IsNotNull(g); }
+
+       
     }
 }
