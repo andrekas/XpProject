57e5436a0c7e3374f11ac2799a678f089af35af6
andrekas <alaurimaa@gmail.com>
2017-02-23 11:18:09 +0200

    muudatused

--- XPProject/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace XPProject.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
--- XPProject/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="XPProject.Global" Language="C#" %>
