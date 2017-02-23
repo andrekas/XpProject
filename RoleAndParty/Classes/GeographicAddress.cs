5c825be4ddf4d0e1121165687198b6a858a52077
andrekas <alaurimaa@gmail.com>
2017-02-23 10:16:03 +0200

    refaktoorisin unused usingud minema

--- RoleAndParty/Classes/GeographicAddress.cs
+++ RoleAndParty/Classes/GeographicAddress.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace RoleAndParty
+﻿namespace RoleAndParty
 {
     public class GeographicAddress
     {
--- RoleAndParty/Classes/WebpageAddresses.cs
+++ RoleAndParty/Classes/WebpageAddresses.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RoleAndParty.Classes
+﻿namespace RoleAndParty.Classes
 {
     public class WebpageAddresses : Archetypes<WebpageAddress>
     {
