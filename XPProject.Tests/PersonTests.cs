bd406898f5cbe38facf81b85707ac3d29ad1e5cf
andrekas <alaurimaa@gmail.com>
2017-03-01 22:30:41 +0200

    gg

--- XPProject.Tests/PersonTests.cs
+++ XPProject.Tests/PersonTests.cs
@@ -6,37 +6,49 @@ namespace XPProject.Tests
     [TestClass]
     public class PersonTests
     {
+
         private Person p;
+
         [TestInitialize]
-        public void Init() { p = new Person(); }
+        public void Init()
+        {
+            p = new Person();
+        }
+
         [TestCleanup]
         public void Cleanup()
         {
             p = null;
         }
-
+        
         [TestMethod]
         public void ConstructorTest()
         {
+
             Assert.IsNotNull(p);
         }
+        
         [TestMethod]
         public void NameTests()
         {
             var fakeValue = "some kind of Name";
+
             p.Name = fakeValue;
             Assert.AreEqual(fakeValue, p.Name);
             Assert.AreEqual(typeof(string), p.Name.GetType());
         }
 
+
         [TestMethod]
         public void GenderTests()
         {
             var fakeValue = "some kind of Gender";
+
             p.Gender = fakeValue;
             Assert.AreEqual(fakeValue, p.Gender);
             Assert.AreEqual(typeof(string), p.Gender.GetType());
         }
+
         [TestMethod]
         public void DateTimeTests()
         {
