1145beab28ba10662980e87ae0dcfc32d50bd9cb
Katjuska <j.podpovedkina@gmail.com>
2017-03-02 08:28:45 +0200

    ,m

--- RoleAndParty/Classes/Person.cs
+++ RoleAndParty/Classes/Person.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using RoleAndParty.Aids;
+using System;
 
 namespace RoleAndParty.Classes
 {
     public class Person
     {
         public string Name { get; set; }
-        public string Gender { get; set; }
+        private string gender;
+        public string Gender
+        {
+            get { return Str.EmptyIfNull(gender); }
+            set { gender = value; }
+        }
         public DateTime DateOfBirth
         {
             get { return DateOfBirth; }
