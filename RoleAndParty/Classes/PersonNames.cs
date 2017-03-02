0d624fb68ec1316bc324fd0b0cd14f175926ece4
Kert Kuusalu <kert.kuusalu@hotmail.com>
2017-03-02 11:21:29 +0200

    peale Viljami juures käimist

---
+++ RoleAndParty/Classes/PersonNames.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoleAndParty.Classes
+{
+    public class PersonNames: Archetypes<PersonName>
+    {
+        public static PersonNames Instances { get; } = new PersonNames();
+
+        public static PersonName GetPersonName(string name)
+        {
+            return Instances.ToList().Find(x => x.Name == name);
+        } 
+    }
+}
